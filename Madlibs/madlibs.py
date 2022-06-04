56d893fd53071356b26075eff3ea87eb60f2608b
ElaniZanlucas <emzan.06@gmail.com>
2022-06-04 15:42:29 -0400

    complete madlibs

--- Madlibs/madlibs.py
+++ Madlibs/madlibs.py
@@ -1,1 +1,11 @@
-print('Hello')
+# String concatenation
+# # Suppose we want to create a string that says "subscribe to "
+# youtuber = ""
+
+# print(f"subscribe to {youtuber}")
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+
+madlib = f"Computer programming is so {adj}! I love to {verb1}. Stay hydrated and {verb2}"
+print(madlib)
